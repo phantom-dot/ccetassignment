c35f547dc6f0261ce7539cd35662d330f79906a8
SAHIL <LOHRANSAHIL@GMAIL.COM>
2023-03-05 17:16:59 +0530

    section1 background change

--- src/components/branches/branches.js
+++ src/components/branches/branches.js
@@ -1,51 +1,58 @@
 import React from 'react'
 import './branches.css'
 import { NavLink } from "react-router-dom";
-import civil from '../../media/civil.png';
-import mech from '../../media/mech.png';
-import cse from '../../media/computer.png';
-import elec from '../../media/elec.png';
+
+// import Tilt from 'react-parallax-tilt';
+// import civil from '../../media/civil.png';
+// import mech from '../../media/mech.png';
+// import cse from '../../media/computer.png';
+// import elec from '../../media/elec.png';
 
 export default function branches() {
   return (
-   <>
-    <div className='branches-main-div'>
+    <> 
+    <div className='branches-outer-div'>
+      <div className='branches-inner-div'>
+      {/* <div className='Title'>
+      <h1>Branches</h1>
+    </div> */}
         <div className='branches'>
-          <div className='card' >
-          <div className='box' id='civil'>
-          <div className = 'content'>
-          <a href='civil.html'>Civil</a>
-
-          </div>
-          </div>
-          </div>
-          <div className='card' >
-          <div class="box" id='ece'>
-          <div class = "content">
-            <a href='ece.html'>ECE</a>
-            
-          </div>
-          </div>
-          </div>
-          <div className='card' >
-          <div class="box" id='mech'>
-          <div class = "content">
-          <a href='mech.html'>Mech</a>
-
-          </div>
-          </div>
-          </div>
-          <div className='card' >
-          <div class="box" id='cse'>
-          <div class = "content">
-          <a href='cse.html'>CSE</a>
-
-          </div>
-          </div>
-          </div>
-            
+          <NavLink to="/Civil"><div className='card' >
+            <div className='box' id='civil'>
+              <div className='content'>
+                <span>Civil</span>
+              </div>
             </div>
+          </div></NavLink>
+
+
+          <NavLink to="/ECE"><div className='card' >
+            <div className='box' id='ece'>
+              <div className='content'>
+                <span>ECE</span>
+              </div>
+            </div>
+          </div></NavLink>
+
+          <NavLink to="/Mech"><div className='card' >
+            <div className='box' id='mech'>
+              <div className='content'>
+                <span>Mech</span>
+              </div>
+            </div>
+          </div></NavLink>
+          <NavLink to="/cse"><div className='card' >
+            <div className='box' id='cse'>
+              <div className='content'>
+                <span>Cse</span>
+              </div>
+            </div>
+          </div></NavLink>
+
         </div>
-   </>
+      </div>
+    </div> 
+  
+    </>
   )
 }
--- src/components/section1/section1.js
+++ src/components/section1/section1.js
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import heroimg from '../../media/heroimg.webp'
 import './/section1.css'
 import Typewriter from 'typewriter-effect';
+// import { CiSquareChevDown } from "react-icons/ci";
+
 export default function section1() {
     return (
         <>
@@ -16,7 +18,7 @@ export default function section1() {
                             {/* <span className='Assignments'>ASSIGNMENTS</span><br /><br /> */}
                             <span className='Assignments'>   <Typewriter
                                 options={{
-                                    strings: ['ASSIGNMENTS','Practical Files','PYQ','Student clubs'],
+                                    strings: ['ASSIGNMENTS', 'Practical Files', 'PYQ', 'Student clubs'],
                                     autoStart: true,
                                     loop: true,
                                 }}
@@ -24,7 +26,7 @@ export default function section1() {
 
                             <span className='made-by'>For all your academic needs 😎</span>
                         </p>
-                        <div>
+                        <div >
                             <button><NavLink to="/register">Explore All Resources ✍️</NavLink></button>
                         </div>
 
@@ -37,7 +39,12 @@ export default function section1() {
 
 
                 </div>
-
+                {/* <div class="arrow-container animated fadeInDown">
+                    <div class="arrow-2">
+                    <FontAwesomeIcon icon="fa-thin fa-arrow-down" />
+                    </div>
+                    <div class="arrow-1 animated hinge infinite zoomIn"></div>
+                </div> */}
             </div>
         </>
     )
