f322954c0acbe472d4c009e7637db5bc24348fd8
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:24 +0530

    Update index.js

--- src/PAGES/index.js
+++ src/PAGES/index.js
@@ -3,10 +3,15 @@ import Section1 from "../components/section1/section1";
 import Branches from "../components/branches/branches";
 import Footer from "../components/footer/footer";
 import NavBar from "../components/Navbar/nav";
+import BasicExample from "../components/Navbar/nav";
+
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = () => {
   return (
     <>
-      <NavBar></NavBar>
+    
+      <NavBar />
       <div>
         <Section1></Section1>
         <Branches></Branches>
@@ -16,4 +21,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Home; 
