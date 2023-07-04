6f1e7e083d752224c996439e6ef62242db6221a2
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:19 +0530

    Update contact.js

--- src/PAGES/contact.js
+++ src/PAGES/contact.js
@@ -1,8 +1,16 @@
 import React from 'react'
+import NavBar from "../components/Navbar/nav"
+import Footer from "../components/footer/footer"
 
 export default function contact() {
   return (
-    <div className='container'> <h1>Contact us</h1>
-    why should u contact us? 🤔</div>
+    <div className='container'> 
+    
+      <NavBar />
+    <h1>Contact us</h1>
+    why should u contact us? 🤔
+
+    <Footer / >
+    </div>
   )
 }
