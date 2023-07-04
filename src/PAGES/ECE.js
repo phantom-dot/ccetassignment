1b6515a383603cefb79595fce4a80b740e00285e
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:50 +0530

    Create ECE.js

---
+++ src/PAGES/ECE.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Sidebar from "../components/sidebar/sidebarcse";
+import NavBar from "../components/Navbar/nav"
+import Footer from "../components/footer/footer"
+const dashboard = () => {
+  return (
+    <>
+        <NavBar />
+
+      <div>
+        <h3>Syllabus ECE </h3>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default dashboard;
+
