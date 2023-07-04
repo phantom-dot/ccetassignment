78caa849db4130315f0deccfd93056c626429a6e
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:06:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Register from "./PAGES/register";
 import Signin from "./PAGES/signin";
 import Contact from "./PAGES/contact";
 import CSE from "./PAGES/CSE";
+import ECE from "./PAGES/ECE";
+import MECH from "./PAGES/MECH";
+import CIVIL from "./PAGES/CIVIL";
+import Books from "./PAGES/books";
+import Notes from "./PAGES/notes";
+
+
+
 
 function App() {
   return (
@@ -16,10 +24,18 @@ function App() {
         <Route path="/sign-in" element={<Signin />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/CSE" element={<CSE />} />
+        <Route path="/ECE" element={<ECE />} />
+        <Route path="/ME" element={<MECH />} />
+        <Route path="/CE" element={<CIVIL />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/notes" element={<Notes />} />
+
+
+
       </Routes>
       
     </Router>
-  );
+  ); 
 }
 
 export default App;
