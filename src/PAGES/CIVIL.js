f8df6115c33d780631eb105b4ad9559a276d3f37
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:08 +0530

    Create CIVIL.js

---
+++ src/PAGES/CIVIL.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Sidebar from "../components/sidebar/sidebarcse";
+import NavBar from "../components/Navbar/nav"
+import Footer from "../components/footer/footer"
+const dashboard = () => {
+  return (
+    <>
+        <NavBar />
+
+      
+      <div>
+        <h3><a href="https://docs.google.com/document/d/1otdkL8qV78j3YeMxd6ytK7guRNrY09Db5HJuRf-KECo/edit?usp=sharing">Syllabus CIVIL </a></h3>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default dashboard;
+
