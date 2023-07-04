e35e56abda40ee83f05e23228769c397bdd09a2b
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:37 +0530

    Create MECH.js

---
+++ src/PAGES/MECH.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Sidebar from "../components/sidebar/sidebarcse";
+import NavBar from "../components/Navbar/nav"
+import Footer from "../components/footer/footer"
+const dashboard = () => {
+  return (
+    <>
+        <NavBar />
+
+      <div>
+        <h3>Syllabus MECH </h3>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default dashboard;
