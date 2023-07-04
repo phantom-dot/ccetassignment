ce6fac9dc7ea3329ed093f8c1aeb5e7f41a449d3
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:21 +0530

    Update CSE.js

--- src/PAGES/CSE.js
+++ src/PAGES/CSE.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Sidebar from "../components/sidebar/sidebarcse";
+import NavBar from "../components/Navbar/nav"
+import Footer from "../components/footer/footer"
 const dashboard = () => {
   return (
     <>
+        <NavBar />
+
       <div>
-        <Sidebar></Sidebar>
+        <h3>Syllabus CSE </h3>
       </div>
+      <Footer />
     </>
   );
 };
