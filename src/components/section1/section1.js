c5c7de64d8988cbd12c8a6ec9edb63a4abe07136
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-05 04:07:04 +0530

    Update section1.js

--- src/components/section1/section1.js
+++ src/components/section1/section1.js
@@ -5,47 +5,47 @@ import './/section1.css'
 import Typewriter from 'typewriter-effect';
 // import { CiSquareChevDown } from "react-icons/ci";
 
+const arr = [
+    "Programming",
+     "Assignments", 
+     "Software",
+     "Documentation",
+     "Engineering", 
+     "Information", 
+     "Exams",
+     "Computer", 
+     "Debugging", 
+     "Design",
+     "Notes", 
+     "Learning",
+    //  "Pressure",
+    // "Structure",
+    // "Theory",
+    // "Laws", 
+    // "Robotics",
+    // "Engineering",
+    // "Hooke",
+    // "Mechanics",
+
+    
+    
+]
+
+
+
+
+const Label = arr.map((a)=>{
+
+    const sty = { gridArea : a }
+
+   return  <div id= {a} style={sty}> <a href='#'>{a}</a></div>
+
+})
+
 export default function section1() {
     return (
-        <>
-
-            <div className='Outer-div'>
-
-                <div className='inner-div'>
-
-                    <div className='text-div'>
-                        <p><span className='ccet-brand' >ccet</span> <br />
-                            {/* <span className='Assignments'>ASSIGNMENTS</span><br /><br /> */}
-                            <span className='Assignments'>   <Typewriter
-                                options={{
-                                    strings: ['ASSIGNMENTS', 'Practical Files', 'PYQ', 'Student clubs'],
-                                    autoStart: true,
-                                    loop: true,
-                                }}
-                            /></span>
-
-                            <span className='made-by'>For all your academic needs 😎</span>
-                        </p>
-                        <div >
-                            <button><NavLink to="/register">Explore All Resources ✍️</NavLink></button>
-                        </div>
-
-                    </div>
-
-
-                    <div className='image-div'>
-                        <img src={heroimg} alt="" />
-                    </div>
-
-
-                </div>
-                {/* <div class="arrow-container animated fadeInDown">
-                    <div class="arrow-2">
-                    <FontAwesomeIcon icon="fa-thin fa-arrow-down" />
-                    </div>
-                    <div class="arrow-1 animated hinge infinite zoomIn"></div>
-                </div> */}
-            </div>
-        </>
-    )
+        <section className='cloud'>
+        {Label}
+        </section>
+    ) 
 }
