d36f0b05457b3fc51816209fb0f51014f9793783
krish-alt <83361261+krish-alt@users.noreply.github.com>
2023-07-07 00:55:17 +0530

    Update nav.js

--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -9,11 +9,11 @@ function DropDown(){
   return  <NavDropdown title="Other.." className='dropdown' id="basic-nav-dropdown">
               
   <div className='dropdown-content'>
-  <NavDropdown.Item href="/notes"><Nav.Link href='/notes'>Notes </Nav.Link></NavDropdown.Item>
-  <NavDropdown.Item href="/books"><Nav.Link href='/books'>  Books</Nav.Link></NavDropdown.Item>
-  <NavDropdown.Item href="#action/3.3"><Nav.Link href='#assignments'>Assignments</Nav.Link></NavDropdown.Item>
+  <NavDropdown.Item href="/notes">Notes </NavDropdown.Item>
+  <NavDropdown.Item href="/books">  Books</NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.3">Assignments</NavDropdown.Item>
   <NavDropdown.Divider />
-  <NavDropdown.Item href="#action/3.4"><Nav.Link href='#contribute'>Contribute ❤️</Nav.Link></NavDropdown.Item>
+  <NavDropdown.Item href="#action/3.4">Contribute ❤️</NavDropdown.Item>
   </div>
   </NavDropdown> 
 }
@@ -23,7 +23,7 @@ function NavBar() {
     <Navbar expand="lg" className="bg-body-tertiary">
     
     <Container className='nav'>
-        <Navbar.Brand href="#home">CCETASSIGNMENT</Navbar.Brand>
+        <Navbar.Brand  className="title" href="/">Jñāna</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
